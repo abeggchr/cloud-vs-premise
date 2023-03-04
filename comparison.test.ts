e517bee12fff44ef5ea6a7618ed52f9811bdafe8
Christian Abegg <Christian.Abegg@zuehlke.com>
2023-03-04 14:40:36 +0100

    remove on-prem

--- comparison.test.ts
+++ comparison.test.ts
@@ -10,36 +10,16 @@ import { ComputeUsageBuilder } from "@cloud-carbon-footprint/core/src/compute/Co
 import { expect, test } from "vitest";
 
 /**
- * Compares a cloud with an on-premise setup in terms of power consumption.
- * 
- * CAUTION: cloud and onPremise setups are not yet comparable.
- * The calculation is purely based on ASSUMPTIONS and WORK IN PROGESS. 
+ * Estimate the emissions using the CCF tool for 12 VMs with the following spec:
+ * - Azure D8s v4
+ * - 8 vCPU / 32 GB
+ * - in Switzerland North
+ * - 50% utilization
+ * - running 1h
  */
-test("cloud vs. on-premise", () => {
-  
-  /**
-   * Calculates the power consumption in kWh for the following server:
-   * - Supermicro high end enterprise server
-   * - X11DPU motherboard
-   * - 2x1000W redundant power supplies (96%)
-   * - average power consumption (measured over 24h): 350W
-   * - xxx cores / xxx threads (assume: cores*threads = 2592)
-   * - xxx GB RAM
-   * - running 1h
-   */
-  function onPremise() {
-    return 0.35;
-  }
+test("cloud compute example", () => {
 
-  /**
-   * The on-premise machine hosts 12 VMs with the following spec:
-   * - Azure D8s v4
-   * - 8 vCPU / 32 GB
-   * - in Switzerland North
-   * - 50% utilization
-   * - running 1h
-   */
-  function cloud() {
+  function estimate() {
     const computeProcessor = INSTANCE_TYPE_COMPUTE_PROCESSOR_MAPPING["D8s v3"];
 
     const constants: CloudConstants = {
@@ -79,11 +59,7 @@ test("cloud vs. on-premise", () => {
     return storageMemoryNetworkFactor * estimation[0].kilowattHours;
   }
 
-  const cloud_kWh = cloud();
-  const onPremise_kWh = onPremise();
+  const cloud_kWh = estimate();
 
-  console.log(`Estimation: cloud ${cloud_kWh.toFixed(2)} vs. on-premise ${onPremise_kWh.toFixed(2)}`);
-
-  expect(cloud_kWh).toBeLessThan(onPremise_kWh);
-  expect(cloud_kWh).toBeGreaterThan(onPremise_kWh * 0.1);
+  console.log(`Estimation: ${cloud_kWh.toFixed(2)}`);
 });
